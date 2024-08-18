375a5b9d5dc841f900fab3a6bebabce0f9e90bd0
Erfan <mrenmityerf@gmail.com>
2024-08-18 15:01:22 +0330

    10

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -3,17 +3,28 @@ import apiClient from "../services/api-client";
 import useGames from "../hooks/useGames";
 import { SimpleGrid } from "@chakra-ui/react";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
       spacing={10}
       padding="10px"
     >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer>
+            <GameCardSkeleton key={skeleton}></GameCardSkeleton>
+          </GameCardContainer>
+        ))}
       {games.map((game) => (
-        <GameCard key={game.id} game={game}></GameCard>
+        <GameCardContainer>
+          <GameCard key={game.id} game={game}></GameCard>
+        </GameCardContainer>
       ))}
     </SimpleGrid>
   );
--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
-export interface Platform{
-    id: number;
-    name:string;
-    slug:string;
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform: Platform}[]
-  metacritic: number
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 interface FetchGamesResponse {
@@ -20,23 +20,31 @@ interface FetchGamesResponse {
   results: Game[];
 }
 
-const useGames = (): { games: Game[]; error: string } => {
+const useGames = (): { games: Game[]; error: string; isLoading: boolean } => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
 
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
-        if (err.code === "ERR_CANCELED") return; // Handle request cancellation
+        if (err.code === "ERR_CANCELED") return; 
         setError(err.message);
+        setLoading(false);
       });
+
+    return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
