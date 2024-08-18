faec1208f1233f2b26e9023de8d6e8723882d3c8
Erfan <mrenmityerf@gmail.com>
2024-08-18 11:09:05 +0330

    6

--- game-hub/src/components/GameGrid.tsx
+++ game-hub/src/components/GameGrid.tsx
@@ -1,29 +1,9 @@
 import React, { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
-
-  if (error) return <p>{error}</p>;
-
+  const { games, error } = useGames();
   return (
     <ul>
       {games.map((game) => (
---
+++ game-hub/src/hooks/useGames.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = (): { games: Game[]; error: string } => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err.code === "ERR_CANCELED") return; // Handle request cancellation
+        setError(err.message);
+      });
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
