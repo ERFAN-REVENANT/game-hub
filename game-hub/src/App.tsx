d11481afe28eed4f0f4e12bca3f8b6dbc0b089d2
Erfan <mrenmityerf@gmail.com>
2024-08-18 09:19:05 +0330

    2

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
-
 
 export default App;
